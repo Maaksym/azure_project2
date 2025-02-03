b1dbd3b59e87026a0fbe07bf7860d7d6672c5885
Maaksym <103188935+Maaksym@users.noreply.github.com>
2025-02-03 23:16:05 +0200

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
 
-# Инициализация базы данных в виде списка словарей
+# Initialization of the database as a list of dictionaries
 db = []
 
 @app.route("/")
